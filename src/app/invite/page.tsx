955bba3d92a03bf13c72d63f498fc00be135ec99
MiguelBurati <buratimiguel@gmail.com>
2025-02-21 22:37:39 -0300

    aula 2
    completa forms

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image'
 import logo from '../../assets/logo.svg'
+import { InviteLinkInput } from './inivte-link-input'
+import { Ranking } from './ranking'
+import { Stats } from './stats'
 
 export default function Invite() {
+	const inviteLink = 'http://localhost:3000/invite/1efaqwfe1231'
+
 	return (
 		<div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
 			<div className="flex flex-col gap-10 w-full max-x-[550px]">
@@ -15,8 +20,24 @@ export default function Invite() {
 						Para entrar no evento, acesse o link enviado para seu e-mail.
 					</p>
 				</div>
-				{/* parei em 08:25 - Aula 2 */}
+
+				<div className="space-y-6">
+					<div className="space-y-3">
+						<h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
+							Indique e ganhe
+						</h2>
+						<p>
+							Convide mais pessoas para o evento e concorra a prêmios
+							exclusivos! É só compartilhar o link abaixo e acompanhar as
+							inscrições:
+						</p>
+					</div>
+
+					<InviteLinkInput inviteLink={inviteLink} />
+					<Stats />
+				</div>
 			</div>
+			<Ranking />
 		</div>
 	)
 }
