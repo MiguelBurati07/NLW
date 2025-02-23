5eea1ad7c6e435725dbc987288521e9b8c6170a3
MiguelBurati <buratimiguel@gmail.com>
2025-02-23 18:36:21 -0300

    completo

--- src/app/invite/[subscriberId]/page.tsx
+++ src/app/invite/[subscriberId]/page.tsx
@@ -1,11 +1,19 @@
 import Image from 'next/image'
-import logo from '../../assets/logo.svg'
+import logo from '../../../assets/logo.svg'
 import { InviteLinkInput } from './inivte-link-input'
 import { Ranking } from './ranking'
 import { Stats } from './stats'
 
-export default function Invite() {
-	const inviteLink = 'http://localhost:3000/invite/1efaqwfe1231'
+interface InvitePageProps {
+	params: Promise<{
+		subscriberId: string
+	}>
+}
+
+export default async function Invite(props: InvitePageProps) {
+	const { subscriberId } = await props.params
+
+	const inviteLink = `http://localhost:3333/invites/${subscriberId}`
 
 	return (
 		<div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
@@ -34,7 +42,7 @@ export default function Invite() {
 					</div>
 
 					<InviteLinkInput inviteLink={inviteLink} />
-					<Stats />
+					<Stats subscriberId={subscriberId} />
 				</div>
 			</div>
 			<Ranking />
